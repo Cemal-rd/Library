19e861d9da26d2b65cb268b9fc9e5bc4f17ba8b4
Cemal Ordu <rdcemal@gmail.com>
2024-02-07 03:56:49 +0300

    Log

--- Library/Controllers/BookController.cs
+++ Library/Controllers/BookController.cs
@@ -4,6 +4,11 @@ using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting.Internal;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.Helpers;
 
 namespace Library.Controllers
 {
@@ -11,119 +16,168 @@ namespace Library.Controllers
     {
         private readonly AppDbContext _appDbContext;
         private readonly IWebHostEnvironment _hostingEnvironment;
+        private readonly Logger _logger;
 
-        public BookController(AppDbContext appDbContext, IWebHostEnvironment hostingEnvironment) 
+        public BookController(AppDbContext appDbContext, IWebHostEnvironment hostingEnvironment)
         {
-            _hostingEnvironment= hostingEnvironment;
-            _appDbContext= appDbContext;
+            _hostingEnvironment = hostingEnvironment;
+            _appDbContext = appDbContext;
+            _logger = new Logger("book_controller_log.txt");
         }
+
         public IActionResult Index()
         {
-            //kitaplar ismine göre alfatik şekilde listelenir
-            var books=_appDbContext.Books.OrderBy(x => x.Title).ToList();
-            return View(books);
+            LogAction("Index");
+            try
+            {
+                //kitaplar ismine göre alfatik şekilde listelenir
+                var books = _appDbContext.Books.OrderBy(x => x.Title).ToList();
+                return View(books);
+            }
+            catch (Exception ex)
+            {
+                LogError("Error in Index method", ex);
+                throw;
+            }
         }
+
         public IActionResult Borrow(int id)
         {
-            var book = _appDbContext.Books.Find(id);
-            if (book != null && book.IsAvailable)
+            LogAction("Borrow");
+            try
             {
-                // Eylemin yapıldığı anın zamanını al
-                DateTime borrowDate = DateTime.Now;
+                var book = _appDbContext.Books.Find(id);
+                if (book != null && book.IsAvailable)
+                {
+                    // Eylemin yapıldığı anın zamanını al
+                    DateTime borrowDate = DateTime.Now;
 
-                // Borrow view'ına kitap bilgilerini taşı
-                return View("Borrow", book);
+                    // Borrow view'ına kitap bilgilerini taşı
+                    return View("Borrow", book);
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Book is not available for borrowing.";
+                    return RedirectToAction("Index");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                TempData["ErrorMessage"] = "Book is not available for borrowing.";
-                return RedirectToAction("Index");
+                LogError("Error in Borrow method", ex);
+                throw;
             }
         }
 
         [HttpPost]
         public IActionResult Borrow(Book model)
         {
-            // Ödünç alınacak kitabın güncellenmiş bilgilerini al
-            var book = _appDbContext.Books.Find(model.Id);
-
-            // Ödünç alınan kitabın bilgilerini güncelle
-            if (book != null)
+            LogAction("Borrow [POST]");
+            try
             {
-                // ReturnDate bilgisini kontrol et
-                if (model.ReturnDate != null && model.ReturnDate > DateTime.Now)
+                // Ödünç alınacak kitabın güncellenmiş bilgilerini al
+                var book = _appDbContext.Books.Find(model.Id);
+
+                // Ödünç alınan kitabın bilgilerini güncelle
+                if (book != null)
                 {
-                    // Ödünç alma işlemi başarılı
-                    book.IsAvailable = false;
-                    book.Borrower = "Borrower Name";
-                    book.BorrowDate = DateTime.Now;
-                    book.ReturnDate = model.ReturnDate;
+                    // ReturnDate bilgisini kontrol et
+                    if (model.ReturnDate != null && model.ReturnDate > DateTime.Now)
+                    {
+                        // Ödünç alma işlemi başarılı
+                        book.IsAvailable = false;
+                        book.Borrower = "Borrower Name";
+                        book.BorrowDate = DateTime.Now;
+                        book.ReturnDate = model.ReturnDate;
 
-                    // Değişiklikleri kaydet
-                    _appDbContext.SaveChanges();
+                        // Değişiklikleri kaydet
+                        _appDbContext.SaveChanges();
 
-                    TempData["SuccessMessage"] = "Book borrowed successfully.";
-                    return RedirectToAction("Index");
+                        TempData["SuccessMessage"] = "Book borrowed successfully.";
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        // Geçersiz iade tarihi
+                        TempData["ErrorMessage"] = "Invalid return date. Please enter a future date.";
+                        return View(model);
+                    }
                 }
                 else
                 {
-                    // Geçersiz iade tarihi
-                    TempData["ErrorMessage"] = "Invalid return date. Please enter a future date.";
-                    return View(model);
+                    TempData["ErrorMessage"] = "Book information not found.";
+                    return RedirectToAction("Index");
                 }
             }
-            else
+            catch (Exception ex)
             {
-                TempData["ErrorMessage"] = "Book information not found.";
-                return RedirectToAction("Index");
+                LogError("Error in Borrow [POST] method", ex);
+                throw;
             }
         }
 
         public IActionResult AddBook()
         {
+            LogAction("AddBook");
             return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> AddBook(Book model, IFormFile imageFile)
         {
-            if (ModelState.IsValid)
+            LogAction("AddBook [POST]");
+            try
             {
-                if (imageFile != null && imageFile.Length > 0)
+                if (ModelState.IsValid)
                 {
-                    // Resim dosyasını yükleme
-                    var uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "img");
-                    var uniqueFileName = Guid.NewGuid().ToString() + "_" + Path.GetFileName(imageFile.FileName);
-                    var filePath = Path.Combine(uploadsFolder, uniqueFileName);
-
-                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    if (imageFile != null && imageFile.Length > 0)
                     {
-                        await imageFile.CopyToAsync(fileStream);
+                        // Resim dosyasını yükleme
+                        var uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "img");
+                        var uniqueFileName = Guid.NewGuid().ToString() + "_" + Path.GetFileName(imageFile.FileName);
+                        var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                        using (var fileStream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await imageFile.CopyToAsync(fileStream);
+                        }
+
+                        // Book nesnesini oluştur ve veritabanına ekle
+                        var book = new Book
+                        {
+                            Title = model.Title,
+                            Author = model.Author,
+                            ImageUrl = uniqueFileName, // Resmin URL'si
+                            IsAvailable = true
+                        };
+
+                        _appDbContext.Books.Add(book);
+                        _appDbContext.SaveChanges();
+
+                        return RedirectToAction("Index");
                     }
-
-                    // Book nesnesini oluştur ve veritabanına ekle
-                    var book = new Book
+                    else
                     {
-                        Title = model.Title,
-                        Author = model.Author,
-                        ImageUrl =  uniqueFileName, // Resmin URL'si
-                        IsAvailable = true
-                    };
-
-                    _appDbContext.Books.Add(book);
-                    _appDbContext.SaveChanges();
-
-                    return RedirectToAction("Index");
+                        ModelState.AddModelError(string.Empty, "Please select a file to upload.");
+                    }
                 }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Please select a file to upload.");
-                }
+
+                return View(model);
             }
+            catch (Exception ex)
+            {
+                LogError("Error in AddBook [POST] method", ex);
+                throw;
+            }
+        }
+        //log işlemleri
+        private void LogAction(string actionName)
+        {
+            _logger.Log($"Executing action: {actionName}");
+        }
 
-            return View(model);
+        private void LogError(string message, Exception ex)
+        {
+            _logger.Log($"ERROR: {message}. Exception: {ex.Message}");
         }
     }
-
-    
 }
---
+++ Library/Helpers/Logger.cs
@@ -0,0 +1,29 @@
+﻿namespace Library.Helpers
+{
+    public class Logger
+    {
+        private readonly string _logFilePath;
+
+        public Logger(string logFilePath)
+        {
+            _logFilePath = logFilePath;
+        }
+
+        public void Log(string message)
+        {
+            try
+            {
+                // Log dosyasına mesajı ekleyin
+                using (StreamWriter writer = File.AppendText(_logFilePath))
+                {
+                    writer.WriteLine($"{DateTime.Now} - {message}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error writing to log file: {ex.Message}");
+            }
+        }
+    }
+}
+
